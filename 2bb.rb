76558f4567937e2a6248d026857db5028287f451
siguajardo <si_guajardo@tamu.edu>
2016-10-21 14:23:03 -0500

    Update 2bb.rb

--- 2bb.rb
+++ 2bb.rb
@@ -2,6 +2,8 @@ require "./way.rb"
 
 day = Hash.new(0)
 
+#This may look the same as other students
+#Worked with Mathew Gutierrez, Nick Proctor, Alex Espinosa, and kathleen Sinor
 File.open("http_access_log" , "r") do |file|
   file.each do |line|
 
