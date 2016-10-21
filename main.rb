e629b7c8a69ddbe959be1b261881cc4e79063a19
siguajardo <si_guajardo@tamu.edu>
2016-10-21 14:22:10 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+#This may look the same as other students
+#Worked with Mathew Gutierrez, Nick Proctor, Alex Espinosa, and kathleen Sinor
 puts "To begin press enter...Hopefully this works when you Clone our repo."
 answer = gets
 
